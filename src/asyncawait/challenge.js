38a0cf694c3f8238b2cf94c72e41a14fefa54afa
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-07-14 19:28:10 -0500

    2

--- src/asyncawait/challenge.js
+++ src/asyncawait/challenge.js
@@ -7,15 +7,31 @@ const anotherFunction = async (url_api) => {
         const character = await fetchData (`${API}${data.results[8].id}`);
         const origin = await fetchData(character.origin.url);
 
-        console.log(data.info.count);
+        //console.log(data.info.count);
         console.log(character.name);
         console.log(origin.dimension);
+        console.log(character.species);
 
     }catch (error){
         console.error(error);
     }
 }
+const otroPersonaje = async (url_api) => {
+    try{
+        const data = await fetchData(url_api)
+        const personaje = await fetchData (`${API}${data.results[0].id}`);
+        console.log(personaje.name);
+        console.log(personaje.status);
+
+    } catch (error){
+        console.error(error);
+
+    }
+
+}
+
 
 console.log('Before');
 anotherFunction(API);
 console.log('After');
+otroPersonaje(API);
